78fb0370b1eb870945d5d1b25379b1607b01182f
ChristiaanSmall <christiaan.small@griffithuni.edu.au>
2023-03-23 17:38:00 +1000

    Initial Commit

---
+++ ChecklistM1/ChecklistM1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistM1App.swift
+//  ChecklistM1
+//
+//  Created by Christiaan Small on 23/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
